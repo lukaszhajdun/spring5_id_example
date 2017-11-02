cac1aaa3aefe42701ac6a43de329e385c8b3b4d4
lukasz <lukasz.hajdun@gmail.com>
2017-11-02 11:46:03 +0100

    Create an Spring Framework DI Example Project using IntelliJ Spring Initializer

---
+++ src/main/java/lukaszhajdun/Spring5IdExampleApplication.java
@@ -0,0 +1,12 @@
+package lukaszhajdun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5IdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5IdExampleApplication.class, args);
+	}
+}
